1d9178d6ef83d7eb17a083f006b9e759894ebc1f
minjekang <kangminje777@gmail.com>
2021-12-13 20:50:56 +0900

    tictactoe

---
+++ tictactoe/src/TicTacToe/TicTacToe.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const TicTacToe = () => {
+    const Cell = () =>{
+        return(
+            <td>-</td>
+        )
+    }
+    return (
+        <div>
+            <table>
+                <tbody>
+                    <tr>
+                        <Cell/>
+                        <Cell/>
+                        <Cell/>
+                    </tr>
+                    <tr>
+                        <Cell/>
+                        <Cell/>
+                        <Cell/>
+                    </tr>
+                    <tr>
+                        <Cell/>
+                        <Cell/>
+                        <Cell/>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default TicTacToe;
