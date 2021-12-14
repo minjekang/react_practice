839a6272856a1a715b27ade0aa062a06953dd6ac
minjekang <kangminje777@gmail.com>
2021-12-14 20:40:11 +0900

    css

--- tictactoe/src/TicTacToe/TicTacToe.js
+++ tictactoe/src/TicTacToe/TicTacToe.js
@@ -2,9 +2,48 @@ import React, { useState } from 'react';
 import './TicTacToe.css'
 const TicTacToe = () => {
     const [turn, setTurn] = useState('X');
-    const [cells, setCells] = useState(Array(9).fill(''));
+    const [cells, setCells] = useState(Array(10).fill(''));
+    const [winner, setWinner] = useState();
 
+    const checkForWinner = (squares) => {
+        let combos = {
+            across: [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9],
+            ],
+            down: [
+                [1,4,7],
+                [2,5,8],
+                [3,6,9],
+            ],
+            diagnol: [
+                [1,5,9],
+                [3,5,7],
+            ],
+        }
+        for(let combo in combos){
+            combos[combo].forEach((pattern) => {
+                if(
+                    squares[pattern[0]] === '' ||
+                    squares[pattern[1]] === '' ||
+                    squares[pattern[2]] === '' 
+                ){}
+                else if (
+                    squares[pattern[0]] === squares[pattern[1]] &&
+                    squares[pattern[1]] === squares[pattern[2]] 
+                ){
+                    setWinner(squares[pattern[0]]);
+                }
+            });
+        }
+    }
     const handleClick = (num) => {
+        if(cells[num] !== ''){
+            alert('다른거 눌러.');
+            return;
+        }
+
         let squares = [...cells];
 
         if (turn === 'X'){
@@ -16,6 +55,7 @@ const TicTacToe = () => {
             setTurn('X');
         }
 
+        checkForWinner(squares);
         setCells(squares);
     }
 
@@ -47,6 +87,12 @@ const TicTacToe = () => {
                     </tr>
                 </tbody>
             </table>
+            {winner && (
+                <>
+                <p>{winner} is win!</p>
+                <button className='re'>Re</button>
+                </>
+            )}
         </div>
     );
 }
