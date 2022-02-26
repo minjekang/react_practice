35f780b074c79a2173322f23a6cd39b95124b71a
minjekang <kangminje777@gmail.com>
2022-02-26 22:18:50 +0900

    first component

--- React/src/App.js
+++ React/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import MyComponent from "./MyComponent";
 import Home from "./Router-pr/Home";
 import TicTactoe from "./TicTacToe/TicTacToe";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <MyComponent />
     </div>
   );
 }
