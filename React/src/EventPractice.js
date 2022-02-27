dc9a206c5f6b89987594d31730997dfd9f24e7ba
minjekang <kangminje777@gmail.com>
2022-02-27 23:46:05 +0900

    function component

--- React/src/EventPractice.js
+++ React/src/EventPractice.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
+/*  클래스형 컴포넌트
 class EventPractice extends Component {
   state = {
     username: "",
@@ -51,5 +52,57 @@ class EventPractice extends Component {
     );
   }
 }
+*/
 
+// 함수형 컴포넌트
+const EventPractice = () => {
+  const [form, setForm] = useState({
+    username: "",
+    message: "",
+  });
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
+  };
+
+  const onClick = () => {
+    alert(username + ":" + message);
+    setForm({
+      username: "",
+      message: "",
+    });
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
+
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해 보세요."
+        value={message}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
 export default EventPractice;
