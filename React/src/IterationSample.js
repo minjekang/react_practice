b690e62348dc1e667bdebea89503c806a5e0041e
minjekang <kangminje777@gmail.com>
2022-03-17 21:02:02 +0900

    add

--- React/src/IterationSample.js
+++ React/src/IterationSample.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IterationSample = () => {
   //   return (
@@ -10,9 +10,33 @@ const IterationSample = () => {
   //     </ul>
   //   );
 
-  const names = ["눈사람", "얼음", "눈", "바람"];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return <ul>{nameList}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>add</button>
+      <ul>{nameList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
