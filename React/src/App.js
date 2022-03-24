aa4fa7195160150a923131028e6c7f070d119cfb
minjekang <kangminje777@gmail.com>
2022-03-24 23:18:26 +0900

    useReducer

--- React/src/App.js
+++ React/src/App.js
@@ -12,19 +12,6 @@ import LifeCycleSample from "./LifeCycleSample";
 import Info from "./Info";
 
 function App() {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Counter />;
 }
 export default App;
--- React/src/Counter.js
+++ React/src/Counter.js
@@ -1,52 +1,29 @@
-import React, { Component } from "react";
+import React, { useReducer } from "react";
 
-class Counter extends Component {
-  /*
-  constructor(props) {
-    super(props);
-    //state의 초깃값 설정
-    this.state = {
-      number: 0,
-      fixedNumber: 0,
-    };
-    */
-  state = {
-    number: 0,
-    fixedNumber: 0,
-  };
-  render() {
-    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회한다.
-    return (
-      <div>
-        <h1>{number}</h1>
-        <h2>바뀌지 않는 값: {fixedNumber}</h2>
-
-        <button
-          onClick={() => {
-            this.setState(
-              {
-                number: number + 1,
-              },
-              () => {
-                console.log("setState 호출.");
-                console.log(this.state);
-              }
-            );
-          }}
-        >
-          +
-        </button>
-
-        <button
-          onClick={() => {
-            this.setState({ number: number - 1 });
-          }}
-        >
-          -
-        </button>
-      </div>
-    );
+function reducer(state, action) {
+  //action.type에 따라 다르 작업 수행
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.value - 1 };
+    default:
+      return state;
   }
 }
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  return (
+    <div>
+      <p>
+        현재 카운터 값은 <b>{state.value}</b>입니다.
+      </p>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+1</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-1</button>
+    </div>
+  );
+};
 
 export default Counter;
