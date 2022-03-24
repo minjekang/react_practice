04443a3d4ca22bdad7c1bfbd1ecc84d73b44a118
minjekang <kangminje777@gmail.com>
2022-03-24 14:10:58 +0900

    useEffect

--- React/src/Info.js
+++ React/src/Info.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
