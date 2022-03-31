38176355c31a156b41b9ace58b7b5915a7d1f622
minjekang <kangminje777@gmail.com>
2022-03-31 20:41:24 +0900

    useRef

--- React/src/RefSample.js
+++ React/src/RefSample.js
@@ -1,18 +1,34 @@
-import React, { Component } from "react";
+import React, { useRef } from "react";
 
-class MyComponent extends Component {
-  id = 1;
-  setId = (n) => {
-    this.id = n;
+// class MyComponent extends Component {
+//   id = 1;
+//   setId = (n) => {
+//     this.id = n;
+//   };
+
+//   printId = () => {
+//     console.log(this.id);
+//   };
+
+//   render() {
+//     return <div>MyComponent</div>;
+//   }
+// }
+
+// export default MyComponent;
+
+// --> useRef 이용
+const RefSample = () => {
+  const id = useRef(1);
+  const setId = (n) => {
+    id.current = n;
   };
 
-  printId = () => {
-    console.log(this.id);
+  const printId = () => {
+    console.log(id.current);
   };
 
-  render() {
-    return <div>MyComponent</div>;
-  }
-}
+  return <div>refSample</div>;
+};
 
-export default MyComponent;
+export default RefSample;
