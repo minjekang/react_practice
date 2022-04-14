de0b35507ef156fe3aa8925d4b99215ed9b1557f
minjekang <kangminje777@gmail.com>
2022-04-14 18:13:32 +0900

    List

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
