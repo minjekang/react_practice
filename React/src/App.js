6e26a1cf5a8b1e8da5a25f99f7b4e07f5ca50413
minjekang <kangminje777@gmail.com>
2022-07-21 17:13:45 +0900

    정리

--- React/src/App.js
+++ React/src/App.js
@@ -14,33 +14,9 @@ import Average from "./Average";
 import RefSample from "./RefSample";
 import SassComponent from "./SassComponent";
 import CSSModule from "./CSSModule";
+import Todo from "./todo/Todo";
 
 const App = () => {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
-  return (
-    <div>
-      <h1>My To dos {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do"
-        />
-        <button>Add to do</button>
-      </form>
-    </div>
-  );
+  return <Todo />;
 };
 export default App;
---
+++ React/src/todo/Todo.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export default function todo() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add to do</button>
+      </form>
+    </div>
+  );
+}
